e0cce93c3ebc76252e7c7629a5ea24c59035bb29
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2022-10-13 02:16:55 +0200

    1.0 + Steckbrief update

---
+++ Aufgaben/01-RandPoem/script/RndPoem.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("working");
+let sub = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let act = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let obj = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+let counter = 6;
+console.log(sub, act, obj);
+for (obj.length > counter; counter > 0; counter--) {
+    console.log(counter);
+    console.log(obj[counter - 1]);
+}
+//# sourceMappingURL=RndPoem.js.map
