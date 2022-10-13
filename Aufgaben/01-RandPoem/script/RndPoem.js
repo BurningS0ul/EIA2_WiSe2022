eafd360c18a844bbbf22249dbfeb7ea9fde7d735
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2022-10-13 15:04:34 +0200

    1.0 Finished

--- Aufgaben/01-RandPoem/script/RndPoem.js
+++ Aufgaben/01-RandPoem/script/RndPoem.js
@@ -1,12 +1,33 @@
 "use strict";
-console.log("working");
-let sub = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-let act = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-let obj = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-let counter = 6;
-console.log(sub, act, obj);
-for (obj.length > counter; counter > 0; counter--) {
-    console.log(counter);
-    console.log(obj[counter - 1]);
-}
+/* Aufgabe: 01-Zufallsgedicht
+Name: Arthur Aguiar Rafael
+Matrikel: 271023
+Datum: 13.10.2022
+Quellen: -
+*/
+var randomPoem;
+(function (randomPoem) {
+    let sub = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let act = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let obj = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    let counter = 6;
+    console.log(sub, act, obj);
+    for (obj.length > counter; counter > 0; counter--) {
+        // let rnd = Math.floor(Math.random());
+        // console.log(counter);
+        // console.log(obj[counter - 1]);
+        let last = getVerse(sub, act, obj);
+        console.log(last);
+    }
+    function getVerse(_sub, _act, _obj) {
+        let rndsub = Math.floor(Math.random() * _sub.length);
+        let rndact = Math.floor(Math.random() * _act.length);
+        let rndobj = Math.floor(Math.random() * _obj.length);
+        let verse = _sub[rndsub] + " " + _act[rndact] + " " + _obj[rndobj] + "!";
+        _sub.splice(rndsub, 1);
+        _act.splice(rndact, 1);
+        _obj.splice(rndobj, 1);
+        return verse;
+    }
+})(randomPoem || (randomPoem = {}));
 //# sourceMappingURL=RndPoem.js.map
--- Aufgaben/01-RandPoem/script/RndPoem.ts
+++ Aufgaben/01-RandPoem/script/RndPoem.ts
@@ -1,13 +1,40 @@
-console.log("working");
+/* Aufgabe: 01-Zufallsgedicht
+Name: Arthur Aguiar Rafael
+Matrikel: 271023
+Datum: 13.10.2022
+Quellen: -
+*/
+namespace randomPoem {
 
-let sub = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-let act = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-let obj = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    let sub: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let act: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let obj: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
-let counter = 6;
+    let counter = 6;
 
-console.log(sub, act, obj);
-for (obj.length > counter; counter > 0; counter--) {
-    console.log(counter);
-    console.log(obj[counter - 1]);
+    console.log(sub, act, obj);
+
+    for (obj.length > counter; counter > 0; counter--) {
+        // let rnd = Math.floor(Math.random());
+        // console.log(counter);
+        // console.log(obj[counter - 1]);
+        let last: string = getVerse(sub, act, obj);
+        console.log(last);
+    }
+
+    function getVerse(_sub: string[], _act: string[], _obj: string[]): string {
+
+        let rndsub: number = Math.floor(Math.random() * _sub.length);
+        let rndact: number = Math.floor(Math.random() * _act.length);
+        let rndobj: number = Math.floor(Math.random() * _obj.length);
+
+        let verse: string = _sub[rndsub] + " " + _act[rndact] + " " + _obj[rndobj] + "!";
+
+        _sub.splice(rndsub, 1);
+        _act.splice(rndact, 1);
+        _obj.splice(rndobj, 1);
+
+        return verse;
+    }
+
 }
