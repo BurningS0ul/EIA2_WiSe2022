6e3c4c718cc360f754d8c2b03e59c94a8822d2f9
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2022-10-29 17:34:09 +0200

    #03 - v.2.0

--- Aufgaben/03-Einkaufsliste/store.js
+++ Aufgaben/03-Einkaufsliste/store.js
@@ -9,15 +9,39 @@ var shoppingList;
 (function (shoppingList) {
     window.addEventListener("load", loadListener);
     function loadListener() {
+        let submit = document.querySelector(".submit");
         let check = document.querySelector(".check");
-        let item = document.querySelector(".item");
-        let store = document.querySelector(".store");
-        check.addEventListener("click", logItems);
-        item.addEventListener("click", logItems);
-        store.addEventListener("click", logItems);
+        let currentdate = document.querySelector(".date");
+        let deleteall = document.querySelector(".delete");
+        let deleted = document.querySelector(".fa-trash-can");
+        currentdate.innerHTML = grabDate();
+        submit.addEventListener("click", logItems);
+        check.addEventListener("click", isChecked);
+        deleteall.addEventListener("click", emptyCart);
+        deleted.addEventListener("click", throwItem);
+    }
+    function emptyCart() {
+        console.log("Emptied the whole Cart!");
+    }
+    function throwItem() {
+        console.log("Threw Item away.");
+    }
+    function isChecked() {
+        console.log("check: clicked");
     }
     function logItems() {
-        console.log();
+        let name = document.querySelector(".name");
+        let num = document.querySelector(".num");
+        let comment = document.querySelector(".comment");
+        let type = document.querySelector(".category");
+        console.log("Added Item: " + num.value + " " + name.value + " " + grabDate() + " " + type.value + " " + comment.value);
+    }
+    function grabDate() {
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        return day + "/" + month + "/" + year;
     }
 })(shoppingList || (shoppingList = {}));
 //# sourceMappingURL=store.js.map
--- Aufgaben/03-Einkaufsliste/store.ts
+++ Aufgaben/03-Einkaufsliste/store.ts
@@ -7,19 +7,48 @@ Quellen: -
 
 namespace shoppingList {
 
-    window.addEventListener("load", loadListener)
+    window.addEventListener("load", loadListener);
 
     function loadListener() {
+        let submit = <HTMLButtonElement>document.querySelector(".submit");
         let check = <HTMLInputElement>document.querySelector(".check");
-        let item = <HTMLDivElement>document.querySelector(".item");
-        let store = <HTMLDivElement>document.querySelector(".store");
+        let currentdate = <HTMLParagraphElement>document.querySelector(".date");
+        let deleteall = <HTMLDivElement>document.querySelector(".delete");
+        let deleted = <HTMLElement>document.querySelector(".fa-trash-can");
 
-        check.addEventListener("click", logItems);
-        item.addEventListener("click", logItems);
-        store.addEventListener("click", logItems);
+        currentdate.innerHTML = grabDate();
+
+        submit.addEventListener("click", logItems);
+        check.addEventListener("click", isChecked);
+        deleteall.addEventListener("click", emptyCart);
+        deleted.addEventListener("click", throwItem);
+    }
+
+    function emptyCart() {
+        console.log("Emptied the whole Cart!");
+    }
+
+    function throwItem() {
+        console.log("Threw Item away.");
+    }
+    function isChecked() {
+        console.log("check: clicked");
     }
 
     function logItems() {
-        console.log();
+        let name = <HTMLInputElement>document.querySelector(".name");
+        let num = <HTMLInputElement>document.querySelector(".num");
+        let comment = <HTMLInputElement>document.querySelector(".comment");
+        let type = <HTMLSelectElement>document.querySelector(".category");
+        console.log("Added Item: " + num.value + " " + name.value + " " + grabDate() + " " + type.value + " " + comment.value);
+
     }
+    function grabDate() {
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        return day + "/" + month + "/" + year;
+    }
+
 }
